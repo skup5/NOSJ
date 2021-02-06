4d72e08dd904ed42012cbe8a450615ad477ca2ed
skup5 <5skup5@seznam.cz>
2021-02-06 02:22:17 +0100

    Text encoding with the given key.
    + encoding text by key from user input
    + Bootstrap 4

--- src/main/kotlin/client.kt
+++ src/main/kotlin/client.kt
@@ -3,11 +3,28 @@ import kotlinx.html.dom.append
 import org.w3c.dom.Node
 import kotlinx.browser.document
 import kotlinx.browser.window
+import kotlinx.html.InputType
+import kotlinx.html.input
+import kotlinx.html.js.onChangeFunction
+import org.w3c.dom.HTMLInputElement
 
 fun main() {
     window.onload = {
-        valentineTitle.also { document.title = it }
-        document.getElementById("root")?.card("key")
+        valentineTitle.also { document.title = HEART + it + HEART }
+        document.getElementById("root")?.let {
+            it.card(plainText)
+            it.append {
+                input(InputType.text, name = "key", classes = "form-control text-align") {
+                    onChangeFunction = { event ->
+                        console.log("${this.name} was changed to")
+                        val target = event.target.unsafeCast<HTMLInputElement>()
+                        console.log(target.value)
+                        document.getElementById("table")?.card("#root input[name=${this.name}]", plainText)
+                    }
+                }
+            }
+        }
+
     }
 }
 
@@ -19,3 +36,4 @@ fun Node.sayHello() {
     }
 }
 
+const val HEART = "\u2764"
--- src/main/kotlin/valentine.kt
+++ src/main/kotlin/valentine.kt
@@ -1,7 +1,10 @@
+import kotlinx.browser.document
 import kotlinx.html.dom.append
 import kotlinx.html.js.table
+import kotlinx.html.js.tbody
 import kotlinx.html.js.td
 import kotlinx.html.js.tr
+import org.w3c.dom.HTMLInputElement
 import org.w3c.dom.Node
 import kotlin.math.ceil
 import kotlin.math.sqrt
@@ -9,21 +12,26 @@ import kotlin.math.sqrt
 const val plainText = "Lorem ipsum etcetera"
 const val valentineTitle = "Krásného Valentýna"
 
-fun Node.card(key: String) {
-    val text = plainText.replace(" ", "")
+fun Node.card(msg: String) {
+    val text = msg.replace(" ", "")
     val rows = ceil(sqrt(text.length.toDouble())).toInt()
     val cols = ceil(text.length.toDouble() / rows).toInt()
     val content = text.toCharArray()
     console.log("rows = $rows, cols = $cols")
+    var index: Int
+    var value: Char
 
     append {
-        table {
-            for (row in 0 until rows) {
-                tr {
-                    for (col in 0 until cols) {
-                        td {
-                            text(row * cols + col)
-//                            text(content[row * cols + col].toString())
+        table(classes = "table table-borderless") {
+            tbody {
+                for (row in 0 until rows) {
+                    tr {
+                        for (col in 0 until cols) {
+                            td {
+                                index = row * cols + col
+                                value = if (index < content.size) content[index] else ' '
+                                text(value.toString())
+                            }
                         }
                     }
                 }
@@ -31,3 +39,41 @@ fun Node.card(key: String) {
         }
     }
 }
+
+fun Node.card(inputKeySelector: String, msg: String) {
+    val text = msg.replace(" ", "")
+    val rows = ceil(sqrt(text.length.toDouble())).toInt()
+    val cols = ceil(text.length.toDouble() / rows).toInt()
+    console.log("rows = $rows, cols = $cols")
+    val content = text.toCharArray()
+    console.log("find $inputKeySelector")
+    val key = document.querySelector(inputKeySelector)?.unsafeCast<HTMLInputElement>()?.value ?: ""
+    console.log("input value: $key")
+    var index: Int
+
+    append {
+        table(classes = "table table-borderless") {
+            tbody {
+                for (row in 0 until rows) {
+                    tr {
+                        for (col in 0 until cols) {
+                            td {
+                                index = row * cols + col
+                                val value = if (index < content.size && key.isNotBlank())
+                                    content[index].code(key[index % key.length])
+                                else ' '
+                                text(value.toString())
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+private fun Char.code(s: Char): Char {
+    return ((orderValue() + s.orderValue()) % 26 + ('A'.toInt())).toChar()
+}
+
+private fun Char.orderValue() = toUpperCase().toInt() - 'A'.toInt()
